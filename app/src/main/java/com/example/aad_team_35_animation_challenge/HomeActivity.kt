c6f616aba3b6078dc285207eca0ce112db112b50
Adedapo <acubedng@yahoo.com>
2019-10-21 23:19:41 +0100

    Merge branch 'branch1' of C:\Users\acube\AndroidStudioProjects\aad-team-35-animation-challenge with conflicts.

---
+++ app/src/main/java/com/example/aad_team_35_animation_challenge/HomeActivity.kt
@@ -0,0 +1,57 @@
+package com.example.aad_team_35_animation_challenge
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.animation.BounceInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.aad_team_35_animation_challenge.app.QuizMoApp.Companion.logout
+import com.example.aad_team_35_animation_challenge.app.QuizMoApp.Companion.animate
+import com.example.aad_team_35_animation_challenge.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityHomeBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        animate(mBinding!!.homeImage, R.animator.scale_animation)
+
+        mBinding!!.takeQuiz.setOnClickListener {
+            interpolate(mBinding!!.takeQuiz)
+            //startActivity(Intent(this@HomeActivity, TakeQuizActivity::class.java))
+        }
+
+        mBinding!!.viewScores.setOnClickListener {
+            interpolate(mBinding!!.viewScores)
+            //startActivity(Intent(this@HomeActivity, ViewScoresActivity::class.java))
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.logout_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout -> logout(this)
+        }
+        return true
+    }
+
+    private fun interpolate(view: View) {
+        val vpa = view.animate()
+        vpa.apply {
+            duration = 50
+            scaleX(1.2f)
+            scaleY(1.2f)
+            interpolator = BounceInterpolator()
+            start()
+        }
+    }
+}
